b0fc8d948a156d839441d1de390988d9bb269f4c
MaikRibeiro <michael.douglas2020@hotmail.com>
2024-12-05 04:10:57 -0300

    feat: Implementation of the logic to find duplicates

--- src/com/javaHelper/DuplicatePropertiesFinder.java
+++ src/com/javaHelper/DuplicatePropertiesFinder.java
@@ -22,6 +22,57 @@ public class DuplicatePropertiesFinder {
 			File file = new File("src/com/resources/" + fileName);
 			scanner = new Scanner(file);
 
+			HashSet<String> hsUniqueKeys = new HashSet<String>();
+			HashSet<String> hsUniqueValues = new HashSet<String>();
+			HashSet<String> hsDuplicateKeys = new HashSet<String>();
+			HashSet<String> hsDuplicateValues = new HashSet<String>();
+			List<String> alValuelessKey = new ArrayList<String>();
+
+			boolean hasDuplicities = false;
+
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				
+				/**
+				 * Fields[0] returns key
+				 * Fields[1] returns value
+				 */
+				String[] fields = line.split("=");
+				
+				if (!hsUniqueKeys.add(fields[0])) {
+					hsDuplicateKeys.add(fields[0].trim());
+					hasDuplicities = true;
+				}
+
+				if (fields.length > 1) {
+
+					if (!hsUniqueValues.add(fields[1])) {
+						hsDuplicateValues.add(fields[1].trim());
+						hasDuplicities = true;
+					}
+
+				} else {
+					alValuelessKey.add(fields[0]);
+				}
+			}
+
+			if (hasDuplicities) {
+				System.out.println("------------Valueless-Key-------------");
+				for (String key : alValuelessKey)
+					System.out.println(key);
+
+				System.out.println("\n------------Duplicate-Keys-------------");
+				for (String key : hsDuplicateKeys)
+					System.out.println(key);
+
+				System.out.println("\n------------Duplicate-Values------------");
+				for (String value : hsDuplicateValues)
+					System.out.println(value);
+				
+			} else {
+				System.out.println("There are no duplicate keys or values");
+			}
+
 		} catch (FileNotFoundException e) {
 			System.out.println(e.getMessage());
 
