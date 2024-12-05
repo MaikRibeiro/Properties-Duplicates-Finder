b0768b8d3c940d6e0da579ce8ff200f9df0caa8f
MaikRibeiro <michael.douglas2020@hotmail.com>
2024-12-05 03:52:18 -0300

    First Commit

---
+++ src/com/javaHelper/DuplicatePropertiesFinder.java
@@ -0,0 +1,12 @@
+package com.javaHelper;
+
+public class DuplicatePropertiesFinder {
+	public void checkFile(String fileName) {
+
+	}
+	
+	public static void main(String[] args) {
+		DuplicatePropertiesFinder duplicatePropertiesFinder = new DuplicatePropertiesFinder();
+		duplicatePropertiesFinder.checkFile("teste.properties");
+	}
+}
