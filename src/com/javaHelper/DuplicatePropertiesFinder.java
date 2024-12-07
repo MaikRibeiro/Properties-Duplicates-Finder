f1015affb82553ea9a5a4df8f51966dd719055de
MaikRibeiro <michael.douglas2020@hotmail.com>
2024-12-07 06:03:08 -0300

    Refatoração: Classes separadas por contexto. Agora são duas classes, uma para encontrar as duplicidades e outra apenas para obter as Keys e Values do .properties

--- src/com/javaHelper/DuplicatePropertiesFinder.java
+++ src/com/javaHelper/DuplicatePropertiesFinder.java
@@ -1,23 +1,16 @@
 package com.javaHelper;
 
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 public class DuplicatePropertiesFinder {
 
-	private final String ROOT_URI = "src/com/resources/";
+	protected static final String ROOT_URI = "src/com/resources/";
 
 	HashSet<String> hsUniqueKeys = new HashSet<String>();
 	HashSet<String> hsUniqueValues = new HashSet<String>();
@@ -102,77 +95,6 @@ public class DuplicatePropertiesFinder {
 		} finally {
 			if (br != null) 
 				try { br.close(); } catch (IOException e) { System.out.println(e.getMessage()); }
-		}
-	}
-
-	public void getKeys(String propertiesFile, boolean addOperatorEqual) {
-
-		if (propertiesFile == null || propertiesFile.isBlank()) {
-			System.err.println("Error: Enter a file name");
-			return;
-		}
-
-		BufferedReader brReader = null;
-		BufferedWriter brWriter = null;
-
-		try {
-			
-			brReader = new BufferedReader(new InputStreamReader(new FileInputStream("src/com/resources/" + propertiesFile)));
-			brWriter = Files.newBufferedWriter(Paths.get(ROOT_URI + "keys.txt"), StandardCharsets.UTF_8);
-			boolean bError = false;
-			
-			String line = "";
-			StringBuilder sbError = new StringBuilder();
-			
-			Set<String> hsDuplicateKey = new HashSet<String>();
-			List<String> alKeys = new ArrayList<String>();
-			
-			while ((line = brReader.readLine()) != null && !line.isBlank()) {
-
-				String[] fields = line.split("=");
-				String key = fields[0].trim();
-				String value = (fields.length > 1) ? fields[1].trim() : null;
-
-				if (!hsDuplicateKey.add(key)) {
-					sbError.append(key).append(" -> ALREDY EXISTS");
-					bError = true;
-				}
-
-				if (value == null) {
-					sbError.append(key).append(" -> CONTENS NO VALUE").append("\n");
-					bError = true;
-				} else {
-
-					if (addOperatorEqual)
-						brWriter.write(key + " =");
-					else
-						brWriter.write(key);
-
-					brWriter.newLine();
-					alKeys.add(key);
-				}
-			}
-
-			if (bError)
-				System.out.println(sbError.toString());
-			else {
-				for (String s : alKeys) {
-					if (addOperatorEqual)
-						System.out.println(s + " =");
-					else
-						System.out.println(s);
-				}
-			}
-
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-
-		} finally {
-			if (brReader != null)
-				try { brReader.close(); } catch (IOException e2) { System.out.println(e2.getMessage()); }
-
-			if (brWriter != null)
-				try { brWriter.close(); } catch (IOException e2) { System.out.println(e2.getMessage()); }
 		}
 	}
 
